462561dd105261d386d209c993894762b5d37de6
Chris Pyle <cpyle@blizzard.com>
2016-09-22 17:11:40 -0700

    Add call and response prayers

--- VoidCultist/core.lua
+++ VoidCultist/core.lua
@@ -1,6 +1,6 @@
 local cooldownTime = time()
-local procChance = 0.05
-local cooldown = 15
+local procChance = 0.04
+local cooldown = 20
 local VCPrefix = "VOID_CULTIST"
 
 local VoidCultist = CreateFrame("Frame")
@@ -10,28 +10,31 @@ VoidCultist:RegisterEvent("CHAT_MSG_ADDON")
 RegisterAddonMessagePrefix(VCPrefix)
 
 local chants = {
-    "Visualize that the seal becomes a gate!",
-    "He is coming! This has been written!",
-    "Turn the key!",
-    "The last prison weakens. The dead sleeper stirs in the depths.",
-    "Untouched by time, unmoved by fate.",
-    "The Black Empire commands the Hour Of Twilight.",
-    "In the land of Ny'alotha there is only sleep.",
-    "At the bottom of the ocean even light must die.",
-    "In the sunken city, he lays dreaming.",
-    "O, discordia!"
+     [1] = {"Visualize that the seal becomes a gate!", "The seal is broken!"},
+     [2] = {"He is coming! This has been written!", "It has been written!"},
+     [3] = {"The lord of ravens will turn the key.", "The gate is open!"},
+     [4] = {"The last prison weakens.", "The dead sleeper stirs in the depths."},
+     [5] = {"Untouched by time, unmoved by fate.", "The Hour of Twilight approaches, unyielding."},
+     [6] = {"In the land of Ny'alotha there is only sleep.", "Only the mad walk amongst the dreaming."},
+     [7] = {"At the bottom of the ocean even light must die.", "The void consumes."},
+     [8] = {"In the sunken city, he lays dreaming.", "N'Zoth slumbers beneath."},
+     [9] = {"O, discordia!", "O, discordia!"},
+    [10] = {"Oblivion offers solace.", "Take the gift! Take it!"}
 }
 
-function VoidCultist:Chant(msg)
+function VoidCultist:Chant(msgIndex)
     broadcast = false
-    if (msg == nil) then
-        msg = chants[math.random(#chants)]
+    if (msgIndex == nil) then
+        msgIndex = math.random(#chants)
+        msg = chants[msgIndex][1]
         broadcast = true        
+    else
+        msg = chants[msgIndex][2]
     end    
     SendChatMessage(msg, "SAY")
     cooldownTime = time()
     if IsAddonMessagePrefixRegistered(VCPrefix) and broadcast then
-        SendAddonMessage(VCPrefix, msg, "RAID")
+        SendAddonMessage(VCPrefix, msgIndex, "RAID")
     end        
 end
 
@@ -49,7 +52,11 @@ end
 function VoidCultist:CHAT_MSG_ADDON(prefix, msg, channel, sender)
     if (prefix == VCPrefix) then
         if (UnitInRange(sender)) then
-            self:Chant(msg)
+            self:Chant(tonumber(msg))
         end
     end
+end
+
+function VCTest()
+    VoidCultist:Chant(1)
 end
